d366481680de1200cecc545a63c1efdbbd9b8218
Palash <palashshah345@gmail.com>
2022-02-14 20:22:54 +0530

    adding cartbean

--- Back End Code/CartBean.java
+++ Back End Code/CartBean.java
@@ -6,17 +6,17 @@ public int getUserid() {
 	return userid;
 }
 
-public void setUserid(int userid) {
-	this.userid = userid;
-}
+//public void setUserid(int userid) {
+//	this.userid = userid;
+//}
 
 //public int getQuantity() {
 //	return quantity;
 //}
-//
-//public void setQuantity(int quantity) {
-//	this.quantity = quantity;
-//}
+
+public void setQuantity(int quantity) {
+	this.quantity = quantity;
+}
 
 
 }
